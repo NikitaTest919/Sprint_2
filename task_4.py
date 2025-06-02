2354df2e988150abb3c714b4b090810ba6a25861
Nikita Popov <popov.nikit2013@yandex.ru>
2025-06-02 15:03:51 +0200

    Внесены доработки по task_4

--- task_4.py
+++ task_4.py
@@ -8,28 +8,30 @@ class EmployeeSalary:
         self.email = email
 
     @classmethod
-    def get_hours(cls, hours, rest_days):
+    def get_hours(cls, name, hours, rest_days, email):
         if hours is None:
             hours = (7 - rest_days) * 8
-        return hours
+        return cls(name, hours, rest_days, email)
 
     @classmethod
-    def get_email(cls, name, email):
+    def get_email(cls, name, hours, rest_days, email):
         if email is None:
             email = f"{name}@email.com"
-        return email
+        return cls(name, hours, rest_days, email)
 
     @classmethod
     def set_hourly_payment(cls, new_payment):
         cls.hourly_payment = new_payment
 
     def salary(self):
-        actual_hours = self.get_hours(self.hours, self.rest_days)
-        return actual_hours * self.hourly_payment
-
+        return self.hours * self.hourly_payment
 
 employee = EmployeeSalary(name="Ivan", rest_days=2)
 
-print(f"Часы работы: {EmployeeSalary.get_hours(employee.hours, employee.rest_days)}")
-print(f"Email: {EmployeeSalary.get_email(employee.name, employee.email)}")
-print(f"Заработная плата: {employee.salary()}")
+employee_with_hours = EmployeeSalary.get_hours(employee.name, employee.hours, employee.rest_days, employee.email)
+print(f"Часы работы: {employee_with_hours.hours}")
+
+employee_with_email = EmployeeSalary.get_email(employee.name, employee.hours, employee.rest_days, employee.email)
+print(f"Email: {employee_with_email.email}")
+
+print(f"Заработная плата: {employee_with_hours.salary()}")
